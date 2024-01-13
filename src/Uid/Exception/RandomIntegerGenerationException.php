4d60c77f8c1053e00a214b8fc7fc25609011db40
Jordi <jmdesarrollo82@gmail.com>
2024-01-13 14:00:34 +0100

    fix: but pass the error code by parameter

--- src/Uid/Exception/RandomIntegerGenerationException.php
+++ src/Uid/Exception/RandomIntegerGenerationException.php
@@ -8,8 +8,8 @@ use Exception;
 
 class RandomIntegerGenerationException extends Exception
 {
-    public function __construct(string $message)
+    public function __construct(string $message, int $code = 500)
     {
-        parent::__construct($message, 500);
+        parent::__construct($message, $code);
     }
 }
